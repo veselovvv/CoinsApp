b75caf659110cd2c2b78eb3e72ee98db41a19dcd
veselovvv <vycheslav5veselov@gmail.com>
2023-06-29 19:14:11 +0300

    Added MainActivity UI

--- app/src/main/java/com/veselovvv/coinsapp/MainActivity.kt
+++ app/src/main/java/com/veselovvv/coinsapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.veselovvv.coinsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.veselovvv.coinsapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragment_container_view)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
