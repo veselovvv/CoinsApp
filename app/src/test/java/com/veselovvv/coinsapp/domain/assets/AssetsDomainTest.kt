f416b4a0477724c9b13848ce88b24ce7e7d7cc48
veselovvv <vycheslav5veselov@gmail.com>
2023-07-02 17:13:56 +0300

    feature/CoinsApp_002_assets Added tests for domain layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/assets/AssetsDomainTest.kt
@@ -0,0 +1,45 @@
+package com.veselovvv.coinsapp.domain.assets
+
+import com.veselovvv.coinsapp.core.ErrorType
+import com.veselovvv.coinsapp.presentation.TestResourceProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AssetsDomainTest {
+    @Test
+    fun test_success() {
+        val assets = listOf(
+            AssetData(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetData(rank = "5", symbol = "USDC", name = "USD Coin")
+        )
+
+        val resultAssets = listOf(
+            AssetDomain(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetDomain(rank = "5", symbol = "USDC", name = "USD Coin")
+        )
+
+        val assetMapper = BaseAssetDomainToUiMapper()
+        val domain = AssetsDomain.Success(assets, BaseAssetDataToDomainMapper())
+        val expected = AssetsUi.Success(resultAssets, assetMapper)
+        val actual = domain.map(BaseAssetsDomainToUiMapper(TestResourceProvider(), assetMapper))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var domain = AssetsDomain.Fail(ErrorType.NO_CONNECTION)
+        var expected = AssetsUi.Fail(NO_CONNECTION_MESSAGE)
+        var actual = domain.map(BaseAssetsDomainToUiMapper(TestResourceProvider(), assetMapper))
+        assertEquals(expected, actual)
+
+        domain = AssetsDomain.Fail(ErrorType.SERVICE_UNAVAILABLE)
+        expected = AssetsUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
+        actual = domain.map(BaseAssetsDomainToUiMapper(TestResourceProvider(), assetMapper))
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
+        private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/assets/BaseAssetsDataToDomainMapperTest.kt
@@ -0,0 +1,34 @@
+package com.veselovvv.coinsapp.domain.assets
+
+import com.veselovvv.coinsapp.core.ErrorType
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+class BaseAssetsDataToDomainMapperTest {
+    private val assetMapper = BaseAssetDataToDomainMapper()
+    private val mapper = BaseAssetsDataToDomainMapper(assetMapper)
+
+    @Test
+    fun test_success() {
+        val assets = listOf(
+            AssetData(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetData(rank = "5", symbol = "USDC", name = "USD Coin")
+        )
+
+        val expected = AssetsDomain.Success(assets, assetMapper)
+        val actual = mapper.map(assets)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var expected = AssetsDomain.Fail(ErrorType.NO_CONNECTION)
+        var actual = mapper.map(UnknownHostException())
+        assertEquals(expected, actual)
+
+        expected = AssetsDomain.Fail(ErrorType.GENERIC_ERROR)
+        actual = mapper.map(Exception())
+        assertEquals(expected, actual)
+    }
+}
