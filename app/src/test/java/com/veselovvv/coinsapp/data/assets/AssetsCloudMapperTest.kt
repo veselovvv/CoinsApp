cd926bc5a6c4d7383b67594ddfe9c5e2a2ba3600
veselovvv <vycheslav5veselov@gmail.com>
2023-07-03 14:30:27 +0300

    feature/CoinsApp_002_assets Added tests for data layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/data/assets/AssetsCloudMapperTest.kt
@@ -0,0 +1,24 @@
+package com.veselovvv.coinsapp.data.assets
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AssetsCloudMapperTest {
+    @Test
+    fun test_mapping() {
+        val mapper = AssetsCloudMapper.Base(ToAssetMapper.Base())
+
+        val assets = listOf(
+            AssetCloud(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetCloud(rank = "5", symbol = "USDC", name = "USD Coin")
+        )
+
+        val expected = listOf(
+            AssetData(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetData(rank = "5", symbol = "USDC", name = "USD Coin")
+        )
+
+        val actual = mapper.map(assets)
+        assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/data/assets/BaseAssetsRepositoryTest.kt
@@ -0,0 +1,74 @@
+package com.veselovvv.coinsapp.data.assets
+
+import com.veselovvv.coinsapp.data.TestException
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BaseAssetsRepositoryTest {
+    @Test
+    fun test_fetch_assets_success() = runBlocking {
+        val repository = AssetsRepository.Base(
+            TestAssetsCloudDataSource(true),
+            AssetsCloudMapper.Base(ToAssetMapper.Base())
+        )
+
+        val expected = AssetsData.Success(
+            listOf(
+                AssetData(rank = "1", symbol = "BTC", name = "Bitcoin"),
+                AssetData(rank = "5", symbol = "USDC", name = "USD Coin")
+            )
+        )
+
+        val actual = repository.fetchAssets()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fetch_assets_fail() = runBlocking {
+        val repository = AssetsRepository.Base(
+            TestAssetsCloudDataSource(false),
+            AssetsCloudMapper.Base(ToAssetMapper.Base())
+        )
+
+        val expected = AssetsData.Fail(TestException(""))
+        val actual = repository.fetchAssets()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_search_assets_success() = runBlocking {
+        val repository = AssetsRepository.Base(
+            TestAssetsCloudDataSource(true),
+            AssetsCloudMapper.Base(ToAssetMapper.Base())
+        )
+
+        val expected = AssetsData.Success(
+            listOf(
+                AssetData(rank = "1", symbol = "BTC", name = "Bitcoin")
+            )
+        )
+
+        val actual = repository.searchAssets(query = "Bitcoin")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_search_assets_fail() = runBlocking {
+        val repository = AssetsRepository.Base(
+            TestAssetsCloudDataSource(true),
+            AssetsCloudMapper.Base(ToAssetMapper.Base())
+        )
+
+        val expected = AssetsData.Success(listOf())
+        val actual = repository.searchAssets(query = "")
+        assertEquals(expected, actual)
+    }
+
+    class TestAssetsCloudDataSource(private val success: Boolean) : AssetsCloudDataSource {
+        override suspend fun fetchAssets() = if (success) listOf(
+            AssetCloud(rank = "1", symbol = "BTC", name = "Bitcoin"),
+            AssetCloud(rank = "5", symbol = "USDC", name = "USD Coin")
+        ) else throw TestException("")
+    }
+}
