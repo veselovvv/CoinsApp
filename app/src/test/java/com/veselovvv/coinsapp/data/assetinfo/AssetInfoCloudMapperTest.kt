382bf2596bf375611eabed19c3ac1a01e71c1e54
veselovvv <vycheslav5veselov@gmail.com>
2023-07-24 19:21:00 +0300

    feature/CoinsApp_003_asset_info Added tests for data layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/data/assetinfo/AssetInfoCloudMapperTest.kt
@@ -0,0 +1,32 @@
+package com.veselovvv.coinsapp.data.assetinfo
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AssetInfoCloudMapperTest {
+    @Test
+    fun test_mapping() {
+        val mapper = AssetInfoCloudMapper.Base(ToAssetInfoMapper.Base())
+        val expected = AssetInfoData(
+            supply = "17193925.0000000000000000",
+            maxSupply = "21000000.0000000000000000",
+            marketCapUsd = "119179791817.6740161068269075",
+            volumeUsd24Hr = "2928356777.6066665425687196",
+            priceUsd = "6931.5058555666618359",
+            changePercent24Hr = "-0.8101417214350335",
+            vwap24Hr = "7175.0663247679233209"
+        )
+        val actual = mapper.map(
+            AssetInfoCloud(
+                supply = "17193925.0000000000000000",
+                maxSupply = "21000000.0000000000000000",
+                marketCapUsd = "119179791817.6740161068269075",
+                volumeUsd24Hr = "2928356777.6066665425687196",
+                priceUsd = "6931.5058555666618359",
+                changePercent24Hr = "-0.8101417214350335",
+                vwap24Hr = "7175.0663247679233209"
+            )
+        )
+        assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/data/assetinfo/BaseAssetInfoRepositoryTest.kt
@@ -0,0 +1,53 @@
+package com.veselovvv.coinsapp.data.assetinfo
+
+import com.veselovvv.coinsapp.data.TestException
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BaseAssetInfoRepositoryTest {
+    @Test
+    fun test_fetch_asset_info_success() = runBlocking {
+        val repository = AssetInfoRepository.Base(
+            TestAssetInfoCloudDataSource(true),
+            AssetInfoCloudMapper.Base(ToAssetInfoMapper.Base())
+        )
+
+        val expected = AssetsInfoData.Success(
+            AssetInfoData(
+                supply = "17193925.0000000000000000",
+                maxSupply = "21000000.0000000000000000",
+                marketCapUsd = "119179791817.6740161068269075",
+                volumeUsd24Hr = "2928356777.6066665425687196",
+                priceUsd = "6931.5058555666618359",
+                changePercent24Hr = "-0.8101417214350335",
+                vwap24Hr = "7175.0663247679233209"
+            )
+        )
+        val actual = repository.fetchAssetInfo(id = "bitcoin")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fetch_asset_info_fail() = runBlocking {
+        val repository = AssetInfoRepository.Base(
+            TestAssetInfoCloudDataSource(false),
+            AssetInfoCloudMapper.Base(ToAssetInfoMapper.Base())
+        )
+        val expected = AssetsInfoData.Fail(TestException(""))
+        val actual = repository.fetchAssetInfo(id = "bitcoin")
+        assertEquals(expected, actual)
+    }
+
+    class TestAssetInfoCloudDataSource(private val success: Boolean) : AssetInfoCloudDataSource {
+        override suspend fun fetchAssetInfo(id: String) = if (success) AssetInfoCloud(
+            supply = "17193925.0000000000000000",
+            maxSupply = "21000000.0000000000000000",
+            marketCapUsd = "119179791817.6740161068269075",
+            volumeUsd24Hr = "2928356777.6066665425687196",
+            priceUsd = "6931.5058555666618359",
+            changePercent24Hr = "-0.8101417214350335",
+            vwap24Hr = "7175.0663247679233209"
+        ) else throw TestException("")
+    }
+}
