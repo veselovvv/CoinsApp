5899eb2f3067665fc64aee924ceeba37723bd3b3
veselovvv <vycheslav5veselov@gmail.com>
2023-08-04 18:29:11 +0300

    feature/CoinsApp_005_asset_markets Added tests for domain layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/assetmarkets/AssetsMarketsDomainTest.kt
@@ -0,0 +1,51 @@
+package com.veselovvv.coinsapp.domain.assetmarkets
+
+import com.veselovvv.coinsapp.core.ErrorType
+import com.veselovvv.coinsapp.presentation.TestResourceProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AssetsMarketsDomainTest {
+    @Test
+    fun test_success() {
+        val assetsMarkets = listOf(
+            AssetMarketsData(exchangeId = "Binance", baseId = "bitcoin", quoteId = "tether"),
+            AssetMarketsData(exchangeId = "Bitfinex", baseId = "bitcoin", quoteId = "united-states-dollar")
+        )
+
+        val resultAssetsMarkets = listOf(
+            AssetMarketsDomain(exchangeId = "Binance", baseId = "bitcoin", quoteId = "tether"),
+            AssetMarketsDomain(exchangeId = "Bitfinex", baseId = "bitcoin", quoteId = "united-states-dollar")
+        )
+
+        val assetMarketsMapper = BaseAssetMarketsDomainToUiMapper()
+        val domain = AssetsMarketsDomain.Success(assetsMarkets, BaseAssetMarketsDataToDomainMapper())
+        val expected = AssetsMarketsUi.Success(resultAssetsMarkets, assetMarketsMapper)
+        val actual = domain.map(
+            BaseAssetsMarketsDomainToUiMapper(TestResourceProvider(), assetMarketsMapper)
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var domain = AssetsMarketsDomain.Fail(ErrorType.NO_CONNECTION)
+        var expected = AssetsMarketsUi.Fail(NO_CONNECTION_MESSAGE)
+        var actual = domain.map(
+            BaseAssetsMarketsDomainToUiMapper(TestResourceProvider(), BaseAssetMarketsDomainToUiMapper)
+        )
+        assertEquals(expected, actual)
+
+        domain = AssetsMarketsDomain.Fail(ErrorType.SERVICE_UNAVAILABLE)
+        expected = AssetsMarketsUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
+        actual = domain.map(
+            BaseAssetsMarketsDomainToUiMapper(TestResourceProvider(), BaseAssetMarketsDomainToUiMapper)
+        )
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
+        private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/assetmarkets/SearchAssetMarketsUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.veselovvv.coinsapp.domain.assetmarkets
+
+import com.veselovvv.coinsapp.core.ErrorType
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+class SearchAssetMarketsUseCaseTest {
+    @Test
+    fun test_success() = runBlocking {
+        val foundAssetsMarkets = listOf(
+            AssetMarketsData(exchangeId = "Binance", baseId = "bitcoin", quoteId = "tether")
+        )
+
+        val assetMarketsMapper = BaseAssetMarketsDataToDomainMapper()
+
+        val useCase = SearchAssetMarketsUseCase.Base(
+            TestAssetMarketsRepository(),
+            BaseAssetsMarketsDataToDomainMapper(assetMarketsMapper)
+        )
+
+        val expected = AssetsMarketsDomain.Success(foundAssetsMarkets, assetMarketsMapper)
+        val actual = useCase.execute(assetId = "bitcoin", query = "Bin")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() = runBlocking {
+        var assetMarketsMapper = BaseAssetMarketsDataToDomainMapper()
+
+        var useCase = SearchAssetMarketsUseCase.Base(
+            TestAssetMarketsRepository(UnknownHostException()),
+            BaseAssetsMarketsDataToDomainMapper(assetMarketsMapper)
+        )
+
+        var expected = AssetsMarketsDomain.Fail(ErrorType.NO_CONNECTION)
+        var actual = useCase.execute(assetId = "bitcoin", query = "Bin")
+        assertEquals(expected, actual)
+
+        assetMarketsMapper = BaseAssetMarketsDataToDomainMapper()
+
+        useCase = SearchAssetMarketsUseCase.Base(
+            TestAssetMarketsRepository(Exception()),
+            BaseAssetsMarketsDataToDomainMapper(assetMarketsMapper)
+        )
+
+        expected = AssetsMarketsDomain.Fail(ErrorType.GENERIC_ERROR)
+        actual = useCase.execute(assetId = "bitcoin", query = "Bin")
+        assertEquals(expected, actual)
+    }
+}
