5eea75aeb3ecd3477e82846657104b4ccfc28481
veselovvv <vycheslav5veselov@gmail.com>
2023-08-09 13:13:15 +0300

    feature/CoinsApp_006_rates Added tests for domain layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/rates/FetchRatesUseCaseTest.kt
@@ -0,0 +1,53 @@
+package com.veselovvv.coinsapp.domain.rates
+
+import com.veselovvv.coinsapp.core.ErrorType
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+class FetchRatesUseCaseTest {
+    @Test
+    fun test_success() = runBlocking {
+        val rates = listOf(
+            RateData(id = "barbadian-dollar", symbol = "BBD", rateUsd = "0.5000000000000000"),
+            RateData(id = "malawian-kwacha", symbol = "MWK", rateUsd = "0.0013750106599420")
+        )
+
+        val rateMapper = BaseRateDataToDomainMapper()
+
+        val useCase = FetchRatesUseCase.Base(
+            TestRatesRepository(),
+            BaseRatesDataToDomainMapper(rateMapper)
+        )
+
+        val expected = RatesDomain.Success(rates, rateMapper)
+        val actual = useCase.execute()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() = runBlocking {
+        var rateMapper = BaseRateDataToDomainMapper()
+
+        var useCase = FetchRatesUseCase.Base(
+            TestRatesRepository(UnknownHostException()),
+            BaseRatesDataToDomainMapper(rateMapper)
+        )
+
+        var expected = RatesDomain.Fail(ErrorType.NO_CONNECTION)
+        var actual = useCase.execute()
+        assertEquals(expected, actual)
+
+        rateMapper = BaseRateDataToDomainMapper()
+
+        useCase = FetchRatesUseCase.Base(
+            TestRatesRepository(Exception()),
+            BaseRatesDataToDomainMapper(rateMapper)
+        )
+
+        expected = RatesDomain.Fail(ErrorType.GENERIC_ERROR)
+        actual = useCase.execute()
+        assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/rates/RatesDomainTest.kt
@@ -0,0 +1,49 @@
+package com.veselovvv.coinsapp.domain.rates
+
+import com.veselovvv.coinsapp.core.ErrorType
+import com.veselovvv.coinsapp.presentation.TestResourceProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RatesDomainTest {
+    @Test
+    fun test_success() {
+        val rates = listOf(
+            RateData(id = "barbadian-dollar", symbol = "BBD", rateUsd = "0.5000000000000000"),
+            RateData(id = "malawian-kwacha", symbol = "MWK", rateUsd = "0.0013750106599420")
+        )
+
+        val resultRates = listOf(
+            RateDomain(id = "barbadian-dollar", symbol = "BBD", rateUsd = "0.5000000000000000"),
+            RateDomain(id = "malawian-kwacha", symbol = "MWK", rateUsd = "0.0013750106599420")
+        )
+
+        val rateMapper = BaseRateDomainToUiMapper()
+        val domain = RatesDomain.Success(rates, BaseRateDataToDomainMapper())
+        val expected = RatesUi.Success(resultRates, rateMapper)
+        val actual = domain.map(BaseRatesDomainToUiMapper(TestResourceProvider(), rateMapper))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var domain = RatesDomain.Fail(ErrorType.NO_CONNECTION)
+        var expected = RatesUi.Fail(NO_CONNECTION_MESSAGE)
+        var actual = domain.map(
+            BaseRatesDomainToUiMapper(TestResourceProvider(), BaseRateDomainToUiMapper())
+        )
+        assertEquals(expected, actual)
+
+        domain = RatesDomain.Fail(ErrorType.SERVICE_UNAVAILABLE)
+        expected = RatesUi.Fail(SERVICE_UNAVAILABLE_MESSAGE)
+        actual = domain.map(
+            BaseRatesDomainToUiMapper(TestResourceProvider(), BaseRateDomainToUiMapper())
+        )
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        private const val NO_CONNECTION_MESSAGE = "No connection. Please try again!"
+        private const val SERVICE_UNAVAILABLE_MESSAGE = "Service unavailable. Please try again!"
+    }
+}
