9898834c9879a581056dfe1db32451b0871076c0
veselovvv <vycheslav5veselov@gmail.com>
2023-08-22 14:43:53 +0300

    feature/CoinsApp_008_exchanges Added tests for data layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/data/exchanges/ExchangesCloudMapperTest.kt
@@ -0,0 +1,24 @@
+package com.veselovvv.coinsapp.data.exchanges
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExchangesCloudMapperTest {
+    @Test
+    fun test_mapping() {
+        val mapper = ExchangesCloudMapper.Base(ToExchangeMapper.Base())
+
+        val exchanges = listOf(
+            ExchangeCloud(id = "okex", name = "Okex", rank = "1"),
+            ExchangeCloud(id = "bithumb", name = "Bithumb", rank = "2")
+        )
+
+        val expected = listOf(
+            ExchangeData(id = "okex", name = "Okex", rank = "1"),
+            ExchangeData(id = "bithumb", name = "Bithumb", rank = "2")
+        )
+
+        val actual = mapper.map(exchanges)
+        assertEquals(expected, actual)
+    }
+}
