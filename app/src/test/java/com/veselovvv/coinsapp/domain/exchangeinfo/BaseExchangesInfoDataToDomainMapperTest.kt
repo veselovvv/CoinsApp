509558841035540710e9d4bf89ec0285943a1ac4
veselovvv <vycheslav5veselov@gmail.com>
2023-09-06 16:59:29 +0300

    feature/CoinsApp_009_exchange_info Added tests for domain layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/exchangeinfo/BaseExchangesInfoDataToDomainMapperTest.kt
@@ -0,0 +1,36 @@
+package com.veselovvv.coinsapp.domain.exchangeinfo
+
+import com.veselovvv.coinsapp.core.ErrorType
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+interface BaseExchangesInfoDataToDomainMapperTest {
+    private val exchangeInfoMapper = BaseExchangeInfoDataToDomainMapper()
+    private val mapper = BaseExchangesInfoDataToDomainMapper(exchangeInfoMapper)
+
+    @Test
+    fun test_success() {
+        val exchangeInfo = ExchangeInfoData(
+            percentTotalVolume = "2.946801735133553120000000000000000000",
+            volumeUsd = "84969370.4499608426167365",
+            tradingPairs = "52",
+            exchangeUrl = "https://kraken.com"
+        )
+
+        val expected = ExchangesInfoDomain.Success(exchangeInfo, exchangeInfoMapper)
+        val actual = mapper.map(exchangeInfo)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() {
+        var expected = ExchangesInfoDomain.Fail(ErrorType.NO_CONNECTION)
+        var actual = mapper.map(UnknownHostException())
+        assertEquals(expected, actual)
+
+        expected = ExchangesInfoDomain.Fail(ErrorType.GENERIC_ERROR)
+        actual = mapper.map(Exception())
+        assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/com/veselovvv/coinsapp/domain/exchangeinfo/FetchExchangeInfoUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.veselovvv.coinsapp.domain.exchangeinfo
+
+import com.veselovvv.coinsapp.core.ErrorType
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.UnknownHostException
+
+class FetchExchangeInfoUseCaseTest {
+    @Test
+    fun test_success() = runBlocking {
+        val exchangeInfo = ExchangeInfoData(
+            percentTotalVolume = "2.946801735133553120000000000000000000",
+            volumeUsd = "84969370.4499608426167365",
+            tradingPairs = "52",
+            exchangeUrl = "https://kraken.com"
+        )
+
+        val exchangeInfoDataToDomainMapper = BaseExchangeInfoDataToDomainMapper()
+        val useCase = FetchExchangeInfoUseCase.Base(
+            TestExchangeInfoRepository(),
+            BaseExchangesInfoDataToDomainMapper(exchangeInfoDataToDomainMapper)
+        )
+        val expected = ExchangesInfoDomain.Success(exchangeInfo, exchangeInfoDataToDomainMapper)
+        val actual = useCase.execute(id = "kraken")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() = runBlocking {
+        val exchangeInfoDataToDomainMapper = BaseExchangeInfoDataToDomainMapper()
+        var useCase = FetchExchangeInfoUseCase.Base(
+            TestExchangeInfoRepository(UnknownHostException()),
+            BaseExchangesInfoDataToDomainMapper(exchangeInfoDataToDomainMapper)
+        )
+        var expected = ExchangesInfoDomain.Fail(ErrorType.NO_CONNECTION)
+        var actual = useCase.execute(id = "kraken")
+        assertEquals(expected, actual)
+
+        useCase = FetchExchangeInfoUseCase.Base(
+            TestExchangeInfoRepository(Exception()),
+            BaseExchangesInfoDataToDomainMapper(exchangeInfoDataToDomainMapper)
+        )
+        expected = ExchangesInfoDomain.Fail(ErrorType.GENERIC_ERROR)
+        actual = useCase.execute(id = "kraken")
+        assertEquals(expected, actual)
+    }
+}
