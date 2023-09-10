025775e1214fd2ae66ff46c12d3ef601a1b11eb2
veselovvv <vycheslav5veselov@gmail.com>
2023-09-10 15:04:57 +0300

    feature/CoinsApp_010_markets Added tests for data layer

---
+++ app/src/test/java/com/veselovvv/coinsapp/data/markets/BaseMarketsRepositoryTest.kt
@@ -0,0 +1,74 @@
+package com.veselovvv.coinsapp.data.markets
+
+import com.veselovvv.coinsapp.data.TestException
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BaseMarketsRepositoryTest {
+    @Test
+    fun test_fetch_markets_success() = runBlocking {
+        val repository = MarketsRepository.Base(
+            TestMarketsCloudDataSource(true),
+            MarketsCloudMapper.Base(ToMarketMapper.Base())
+        )
+
+        val expected = MarketsData.Success(
+            listOf(
+                MarketData(exchangeId = "bitstamp", baseSymbol = "BTC", quoteSymbol = "USD"),
+                MarketData(exchangeId = "zb", baseSymbol = "DASH", quoteSymbol = "USDT")
+            )
+        )
+
+        val actual = repository.fetchMarkets()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fetch_markets_fail() = runBlocking {
+        val repository = MarketsRepository.Base(
+            TestMarketsCloudDataSource(false),
+            MarketsCloudMapper.Base(ToMarketMapper.Base())
+        )
+
+        val expected = MarketsData.Fail(TestException(""))
+        val actual = repository.fetchMarkets()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_search_markets_success() = runBlocking {
+        val repository = MarketsRepository.Base(
+            TestMarketsCloudDataSource(true),
+            MarketsCloudMapper.Base(ToMarketMapper.Base())
+        )
+
+        val expected = MarketsData.Success(
+            listOf(
+                MarketData(exchangeId = "bitstamp", baseSymbol = "BTC", quoteSymbol = "USD")
+            )
+        )
+
+        val actual = repository.searchMarkets(query = "bitstamp")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_search_markets_fail() = runBlocking {
+        val repository = MarketsRepository.Base(
+            TestMarketsCloudDataSource(true),
+            MarketsCloudMapper.Base(ToMarketMapper.Base())
+        )
+
+        val expected = MarketsData.Success(listOf())
+        val actual = repository.searchMarkets(query = "Az")
+        assertEquals(expected, actual)
+    }
+
+    class TestMarketsCloudDataSource(private val success: Boolean) : MarketsCloudDataSource {
+        override suspend fun fetchMarkets() = if (success) listOf(
+            MarketCloud(exchangeId = "bitstamp", baseSymbol = "BTC", quoteSymbol = "USD"),
+            MarketCloud(exchangeId = "zb", baseSymbol = "DASH", quoteSymbol = "USDT")
+        ) else throw TestException("")
+    }
+}
