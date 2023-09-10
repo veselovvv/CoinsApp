486d4056289e40a0007a233b73ac7ded70746ec0
veselovvv <vycheslav5veselov@gmail.com>
2023-09-10 13:17:02 +0300

    feature/CoinsApp_009_exchange_info Fixed nullable parameters in ExchangeInfoCloud

--- app/src/main/java/com/veselovvv/coinsapp/data/exchangeinfo/cloud/ExchangeInfoCloud.kt
+++ app/src/main/java/com/veselovvv/coinsapp/data/exchangeinfo/cloud/ExchangeInfoCloud.kt
@@ -7,14 +7,23 @@ import com.veselovvv.coinsapp.data.exchangeinfo.ToExchangeInfoMapper
 
 data class ExchangeInfoCloud(
     @SerializedName("percentTotalVolume")
-    private val percentTotalVolume: String,
+    private val percentTotalVolume: String?,
     @SerializedName("volumeUsd")
-    private val volumeUsd: String,
+    private val volumeUsd: String?,
     @SerializedName("tradingPairs")
     private val tradingPairs: String,
     @SerializedName("exchangeUrl")
     private val exchangeUrl: String
 ) : Object<ExchangeInfoData, ToExchangeInfoMapper> {
     override fun map(mapper: ToExchangeInfoMapper) =
-        mapper.map(percentTotalVolume, volumeUsd, tradingPairs, exchangeUrl)
+        mapper.map(
+            percentTotalVolume ?: NO_DATA,
+            volumeUsd ?: NO_DATA,
+            tradingPairs,
+            exchangeUrl
+        )
+
+    companion object {
+        private const val NO_DATA = "-"
+    }
 }
