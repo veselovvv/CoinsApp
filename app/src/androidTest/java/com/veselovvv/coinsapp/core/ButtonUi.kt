cd8b278b2618a9358ed9e366a341c651ab4860cb
veselovvv <vycheslav5veselov@gmail.com>
2024-09-01 20:54:13 +0300

    Added asset history ui objects

--- app/src/androidTest/java/com/veselovvv/coinsapp/core/ButtonUi.kt
+++ app/src/androidTest/java/com/veselovvv/coinsapp/core/ButtonUi.kt
@@ -2,6 +2,7 @@ package com.veselovvv.coinsapp.core
 
 import android.widget.LinearLayout
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
@@ -12,13 +13,19 @@ import com.google.android.material.button.MaterialButton
 import org.hamcrest.CoreMatchers.allOf
 
 class ButtonUi {
+    fun interaction(buttonId: Int) = onView(
+        allOf(
+            withParent(isAssignableFrom(LinearLayout::class.java)),
+            withId(buttonId),
+            isAssignableFrom(MaterialButton::class.java)
+        )
+    )
+
     fun checkIsVisible(buttonId: Int) {
-        onView(
-            allOf(
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withId(buttonId),
-                isAssignableFrom(MaterialButton::class.java)
-            )
-        ).perform(scrollTo()).check(matches(isDisplayed()))
+        interaction(buttonId).perform(scrollTo()).check(matches(isDisplayed()))
+    }
+
+    fun click(buttonId: Int) {
+        interaction(buttonId).perform(click())
     }
 }
