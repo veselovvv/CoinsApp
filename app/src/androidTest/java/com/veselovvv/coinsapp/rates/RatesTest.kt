93ec0258a5d872de4aad618e55ee5419ab15aae4
veselovvv <vycheslav5veselov@gmail.com>
2024-09-04 11:35:07 +0300

    Added load asset info and go back ui test

--- app/src/androidTest/java/com/veselovvv/coinsapp/rates/RatesTest.kt
+++ app/src/androidTest/java/com/veselovvv/coinsapp/rates/RatesTest.kt
@@ -1,5 +1,6 @@
 package com.veselovvv.coinsapp.rates
 
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.veselovvv.coinsapp.MainActivity
@@ -91,7 +92,6 @@ class RatesTest {
         }
     }
 
-    // TODO RUN
     /**
      * Check Assets Page is visible
      * Check assets list state
@@ -187,6 +187,127 @@ class RatesTest {
             )
 
             clickBackSearchButton()
+            checkRatesListState(
+                rates = listOf(
+                    Triple("BMD", "bermudan-dollar", "1.0000000000000000"),
+                    Triple("NOK", "norwegian-krone", "0.0944781311469834"),
+                    Triple("HKD", "hong-kong-dollar", "0.1282511835339845")
+                )
+            )
+        }
+    }
+
+    /**
+     * Check Assets Page is visible
+     * Check assets list state
+     * 1. Click on "Rates" tab in BottomNavigation
+     * Check Rates Page is visible
+     * Check rates list state
+     * 2. Click on first item in list (index = 0)
+     * Check Rate Info Page is visible
+     * Check rate info state
+     * 3. Recreate activity
+     * Check error state with text "No connection. Please try again!"
+     * 4. Click "Try again" button
+     * Check Rate Info Page is visible
+     * Check rate info state
+     * 5. Swipe to refresh
+     * Check error state with text "No connection. Please try again!"
+     * 6. Click "Try again" button
+     * Check Rate Info Page is visible
+     * Check rate info state
+     * 7. Press back button
+     * Check Rate Info Page is not visible
+     * Check Rates Page is visible
+     * Check error state with text "No connection. Please try again!"
+     * 8. Click "Try again" button
+     * Check Rate Info Page is not visible
+     * Check Rates Page is visible
+     * Check rates list state
+     */
+    @Test
+    fun loadAssetInfoAndGoBack() {
+        with(AssetsPage()) {
+            checkIsVisible()
+            checkAssetsListState(
+                assets = listOf(
+                    Triple("BTC", "Bitcoin", "1"),
+                    Triple("ETH", "Ethereum", "2"),
+                    Triple("USDT", "Tether", "3")
+                )
+            )
+
+            clickOnRatesTab()
+        }
+
+        val ratesPage = RatesPage()
+
+        with(ratesPage) {
+            checkIsVisible()
+            checkRatesListState(
+                rates = listOf(
+                    Triple("BMD", "bermudan-dollar", "1.0000000000000000"),
+                    Triple("NOK", "norwegian-krone", "0.0944781311469834"),
+                    Triple("HKD", "hong-kong-dollar", "0.1282511835339845")
+                )
+            )
+
+            clickOnItemInList(index = 0)
+        }
+
+        val rateInfoPage = RateInfoPage()
+
+        with(rateInfoPage) {
+            checkIsVisible()
+            checkRateInfoState(
+                symbol = "BMD",
+                name = "bermudan-dollar",
+                currencySymbol = "B",
+                type = "crypto",
+                rateUsd = "1.0000000000000000"
+            )
+
+            activityScenarioRule.scenario.recreate()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+            checkIsVisible()
+            checkRateInfoState(
+                symbol = "BMD",
+                name = "bermudan-dollar",
+                currencySymbol = "B",
+                type = "crypto",
+                rateUsd = "1.0000000000000000"
+            )
+
+            swipeToRefresh()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+            checkIsVisible()
+            checkRateInfoState(
+                symbol = "BMD",
+                name = "bermudan-dollar",
+                currencySymbol = "B",
+                type = "crypto",
+                rateUsd = "1.0000000000000000"
+            )
+        }
+
+        pressBack()
+        rateInfoPage.checkIsNotVisible()
+
+        with(ratesPage) {
+            checkIsVisible()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+        }
+
+        rateInfoPage.checkIsNotVisible()
+
+        with(ratesPage) {
+            checkIsVisible()
             checkRatesListState(
                 rates = listOf(
                     Triple("BMD", "bermudan-dollar", "1.0000000000000000"),
