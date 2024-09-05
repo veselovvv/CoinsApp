d82ea30ea88664e9147babaea41e0b4f90e7de74
veselovvv <vycheslav5veselov@gmail.com>
2024-09-05 10:53:59 +0300

    Added load exchange info and go back ui test

--- app/src/androidTest/java/com/veselovvv/coinsapp/exchanges/ExchangesTest.kt
+++ app/src/androidTest/java/com/veselovvv/coinsapp/exchanges/ExchangesTest.kt
@@ -1,5 +1,6 @@
 package com.veselovvv.coinsapp.exchanges
 
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.veselovvv.coinsapp.MainActivity
@@ -186,6 +187,130 @@ class ExchangesTest {
             )
 
             clickBackSearchButton()
+            checkExchangesListState(
+                exchanges = listOf(
+                    Triple("Binance", "binance", "1"),
+                    Triple("Crypto.com Exchange", "crypto", "2"),
+                    Triple("Coinbase Pro", "gdax", "3")
+                )
+            )
+        }
+    }
+
+    /**
+     * Check Assets Page is visible
+     * Check assets list state
+     * 1. Click on "Exchanges" tab in BottomNavigation
+     * Check Exchanges Page is visible
+     * Check exchanges list state
+     * 2. Click on first item in list (index = 0)
+     * Check Exchange Info Page is visible
+     * Check exchange info state
+     * 3. Recreate activity
+     * Check error state with text "No connection. Please try again!"
+     * 4. Click "Try again" button
+     * Check Exchange Info Page is visible
+     * Check exchange info state
+     * 5. Swipe to refresh
+     * Check error state with text "No connection. Please try again!"
+     * 6. Click "Try again" button
+     * Check Exchange Info Page is visible
+     * Check exchange info state
+     * 7. Press back button
+     * Check Exchange Info Page is not visible
+     * Check Exchanges Page is visible
+     * Check error state with text "No connection. Please try again!"
+     * 8. Click "Try again" button
+     * Check Exchange Info Page is not visible
+     * Check Exchanges Page is visible
+     * Check exchanges list state
+     */
+    @Test
+    fun loadExchangeInfoAndGoBack() {
+        with(AssetsPage()) {
+            checkIsVisible()
+            checkAssetsListState(
+                assets = listOf(
+                    Triple("BTC", "Bitcoin", "1"),
+                    Triple("ETH", "Ethereum", "2"),
+                    Triple("USDT", "Tether", "3")
+                )
+            )
+
+            clickOnExchangesTab()
+        }
+
+        val exchangesPage = ExchangesPage()
+
+        with(exchangesPage) {
+            checkIsVisible()
+            checkExchangesListState(
+                exchanges = listOf(
+                    Triple("Binance", "binance", "1"),
+                    Triple("Crypto.com Exchange", "crypto", "2"),
+                    Triple("Coinbase Pro", "gdax", "3")
+                )
+            )
+
+            clickOnItemInList(index = 0)
+        }
+
+        val exchangeInfoPage = ExchangeInfoPage()
+
+        with(exchangeInfoPage) {
+            checkIsVisible()
+            checkExchangeInfoState(
+                name = "Binance",
+                rank = "1",
+                percentTotalVolume = "2.946801735133553120000000000000000000",
+                volumeUsd = "84969370.4499608426167365",
+                traidingPairs = "52",
+                exchangeUrl = "https://kraken.com"
+            )
+
+            activityScenarioRule.scenario.recreate()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+            checkIsVisible()
+            checkExchangeInfoState(
+                name = "Binance",
+                rank = "1",
+                percentTotalVolume = "2.946801735133553120000000000000000000",
+                volumeUsd = "84969370.4499608426167365",
+                traidingPairs = "52",
+                exchangeUrl = "https://kraken.com"
+            )
+
+            swipeToRefresh()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+            checkIsVisible()
+            checkExchangeInfoState(
+                name = "Binance",
+                rank = "1",
+                percentTotalVolume = "2.946801735133553120000000000000000000",
+                volumeUsd = "84969370.4499608426167365",
+                traidingPairs = "52",
+                exchangeUrl = "https://kraken.com"
+            )
+        }
+
+        pressBack()
+        exchangeInfoPage.checkIsNotVisible()
+
+        with(exchangesPage) {
+            checkIsVisible()
+            checkErrorState(message = "No connection. Please try again!")
+
+            clickTryAgainButton()
+        }
+
+        exchangeInfoPage.checkIsNotVisible()
+
+        with(exchangesPage) {
+            checkIsVisible()
             checkExchangesListState(
                 exchanges = listOf(
                     Triple("Binance", "binance", "1"),
