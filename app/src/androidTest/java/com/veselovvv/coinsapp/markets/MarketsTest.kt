b269f00532bec4fcefaeac312c0acae7847b62d7
veselovvv <vycheslav5veselov@gmail.com>
2024-09-05 15:06:28 +0300

    Added search markets ui test

--- app/src/androidTest/java/com/veselovvv/coinsapp/markets/MarketsTest.kt
+++ app/src/androidTest/java/com/veselovvv/coinsapp/markets/MarketsTest.kt
@@ -90,4 +90,113 @@ class MarketsTest {
             )
         }
     }
+
+    /**
+     * Check Assets Page is visible
+     * Check assets list state
+     * 1. Click on "Markets" tab in BottomNavigation
+     * Check Markets Page is visible
+     * Check markets list state
+     * 2. Click search button
+     * Check search view state
+     * 3. CLick back search button
+     * Check markets list state
+     * 4. Click search button
+     * Check search view state
+     * 5. Type "alter" in search view
+     * Check markets list state with found rate
+     * 6. CLick back search button
+     * Check markets list state
+     * 7. Click search button
+     * Check search view state
+     * 8. Type "Smth" in search view
+     * Check no results state with text "No results to show"
+     * 9. Type "alter" in search view
+     * Check markets list state with found rate
+     * 10. CLick back search button
+     * Check markets list state
+     */
+    @Test
+    fun searchMarkets() {
+        with(AssetsPage()) {
+            checkIsVisible()
+            checkAssetsListState(
+                assets = listOf(
+                    Triple("BTC", "Bitcoin", "1"),
+                    Triple("ETH", "Ethereum", "2"),
+                    Triple("USDT", "Tether", "3")
+                )
+            )
+
+            clickOnMarketsTab()
+        }
+
+        with(MarketsPage()) {
+            checkIsVisible()
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+
+            clickSearchButton()
+            checkSearchViewState()
+
+            clickBackSearchButton()
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+
+            clickSearchButton()
+            checkSearchViewState()
+
+            typeInSearchView(text = "alter")
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+
+            clickBackSearchButton()
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+
+            clickSearchButton()
+            checkSearchViewState()
+
+            typeInSearchView(text = "Smth")
+            checkNoResultsState(text = "No results to show")
+
+            typeInSearchView(text = "alter")
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+
+            clickBackSearchButton()
+            checkMarketsListState(
+                markets = listOf(
+                    Triple("alterdice", "BTC", "USDT"),
+                    Triple("alterdice", "LTC", "USDT"),
+                    Triple("alterdice", "FTM", "USDT")
+                )
+            )
+        }
+    }
 }
