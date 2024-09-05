fa13789037fc34206660f50e185fd4d1df9388d8
veselovvv <vycheslav5veselov@gmail.com>
2024-09-05 15:08:46 +0300

    Added markets page object

--- app/src/androidTest/java/com/veselovvv/coinsapp/markets/MarketsPage.kt
+++ app/src/androidTest/java/com/veselovvv/coinsapp/markets/MarketsPage.kt
@@ -2,10 +2,12 @@ package com.veselovvv.coinsapp.markets
 
 import com.veselovvv.coinsapp.R
 import com.veselovvv.coinsapp.core.AbstractPage
+import com.veselovvv.coinsapp.core.SearchViewUi
 import com.veselovvv.coinsapp.core.SwipeToRefreshUi
 
 class MarketsPage : AbstractPage(R.id.markets_root_layout) {
     private val recyclerViewUi = MarketsRecyclerViewUi()
+    private val searchViewUi = SearchViewUi()
     private val swipeToRefreshUi = SwipeToRefreshUi(
         R.id.markets_root_layout,
         R.id.markets_swipe_to_refresh
@@ -17,4 +19,9 @@ class MarketsPage : AbstractPage(R.id.markets_root_layout) {
     fun swipeToRefresh() = swipeToRefreshUi.swipeToRefresh()
     fun checkErrorState(message: String) = recyclerViewUi.checkErrorState(message = message)
     fun clickTryAgainButton() = recyclerViewUi.clickTryAgainButton()
+    fun clickSearchButton() = searchViewUi.clickSearchButton(searchMenuItemId = R.id.action_search_markets)
+    fun checkSearchViewState() = searchViewUi.checkSearchViewState()
+    fun clickBackSearchButton() = searchViewUi.clickBackSearchButton()
+    fun typeInSearchView(text: String) = searchViewUi.typeInSearchView(text = text)
+    fun checkNoResultsState(text: String) = recyclerViewUi.checkNoResultsState(text = text)
 }
